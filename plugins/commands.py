3feff6ce81c126d69aac102cc5b24528f4fcf037
RazAryanBot <136279522+RazAryanBot@users.noreply.github.com>
2023-07-19 23:05:35 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -936,11 +936,11 @@ async def deletemultiplefiles(bot, message):
     try:
         keyword = message.text.split(" ", 1)[1]
     except:
-        return await message.reply_text(f"<b>Hey {message.from_user.mention}, Give me a keyword along with the command to delete files.</b>")
-    k = await bot.send_message(chat_id=message.chat.id, text=f"<b>Fetching Files for your query {keyword} on DB... Please wait...</b>")
+        return await message.reply_text(f"<b>Hᴇʏ {message.from_user.mention}, Gɪᴠᴇ Mᴇ A Kᴇʏᴡᴏʀᴅ Aʟᴏɴɢ Wɪᴛʜ Tʜᴇ Cᴏᴍᴍᴀɴᴅ Tᴏ Dᴇʟᴇᴛᴇ Fɪʟᴇꜱ.</b>")
+    k = await bot.send_message(chat_id=message.chat.id, text=f"<b>Fᴇᴛᴄʜɪɴɢ Fɪʟᴇꜱ Fᴏʀ Yᴏᴜʀ Qᴜᴇʀʏ {keyword} Oɴ DB... Pʟᴇᴀꜱᴇ Wᴀɪᴛ...</b>")
     files, total = await get_bad_files(keyword)
     await k.delete()
-    #await k.edit_text(f"<b>Found {total} files for your query {keyword} !\n\nFile deletion process will start in 5 seconds !</b>")
+    #await k.edit_text(f"<b>Fᴏᴜɴᴅ {total} Fɪʟᴇꜱ Fᴏʀ Yᴏᴜʀ Qᴜᴇʀʏ {keyword} !\n\nDᴏ Yᴏᴜ Wᴀɴᴛ Tᴏ Dᴇʟᴇᴛᴇ?</b>")
     #await asyncio.sleep(5)
     btn = [[
        InlineKeyboardButton("Yᴇꜱ, Cᴏɴᴛɪɴᴜᴇ !", callback_data=f"killfilesdq#{keyword}")
@@ -948,7 +948,7 @@ async def deletemultiplefiles(bot, message):
        InlineKeyboardButton("Nᴏ, Aʙᴏʀᴛ Oᴘᴇʀᴀᴛɪᴏɴ !", callback_data="close_data")
     ]]
     await message.reply_text(
-        text=f"<b>Found {total} files for your query {keyword} !\n\nDo you want to delete?</b>",
+        text=f"<b>Fᴏᴜɴᴅ {total} Fɪʟᴇꜱ Fᴏʀ Yᴏᴜʀ Qᴜᴇʀʏ {keyword} !\n\nDᴏ Yᴏᴜ Wᴀɴᴛ Tᴏ Dᴇʟᴇᴛᴇ?</b>",
         reply_markup=InlineKeyboardMarkup(btn),
         parse_mode=enums.ParseMode.HTML
     )
