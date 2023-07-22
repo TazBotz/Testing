4522ac1f8f6442994c2c7e42b50b5cf8f6741cd6
RazAryanBot <136279522+RazAryanBot@users.noreply.github.com>
2023-07-22 21:00:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -285,7 +285,7 @@ async def advantage_spoll_choker(bot, query):
                 ]]
                 if NO_RESULTS_MSG:
                     await bot.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, movie)))
-                k = await query.message.edit(script.MVE_NT_FND
+                k = await query.message.edit(script.MVE_NT_FND, 
                 reply_markup=InlineKeyboardMarkup(button)
                 )
                 await asyncio.sleep(10)
