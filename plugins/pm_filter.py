a4be3d5d09c1ae5bcb7601696148d0542e0d24b7
RazAryanBot <136279522+RazAryanBot@users.noreply.github.com>
2023-07-22 21:12:17 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -281,7 +281,7 @@ async def advantage_spoll_choker(bot, query):
                 reqstr1 = query.from_user.id if query.from_user else 0
                 reqstr = await bot.get_users(reqstr1)
                 button = [[
-                   InlineKeyboardButton("📜 Rᴇǫᴜᴇꜱᴛ Hᴇʀᴇ 📜", url="https://t.me/{SUPPORT_CHAT}")
+                   InlineKeyboardButton("📜 Rᴇǫᴜᴇꜱᴛ Hᴇʀᴇ 📜", url=SUPPORT_CHAT)
                 ]]
                 if NO_RESULTS_MSG:
                     await bot.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, movie)))
