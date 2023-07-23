52e2a935e14d5d0c60b083ca2fb1298dc9ada3d8
RazAryanBot <136279522+RazAryanBot@users.noreply.github.com>
2023-07-23 20:05:46 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -69,7 +69,7 @@ async def give_filter(client, message):
             return await message.reply_text(
                 f"<b>Hᴇʏ {message.from_user.mention}, \n\n{str(total_results)}  Rᴇsᴜʟᴛs Aʀᴇ Aʟʀᴇᴀᴅʏ Aᴠᴀɪʟᴀʙʟᴇ Fᴏʀ {search}.\n\nTʜɪs Is A Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ\nSᴏ I Cᴀɴ'ᴛ Gɪᴠᴇ Fɪʟᴇ Hᴇʀᴇ.\n\nPʟᴇᴀꜱᴇ Sᴇᴀʀᴄʜ Iᴛ Oɴ Oᴜʀ\nMᴏᴠɪᴇ Gʀᴏᴜᴘ.👇</b>",
                 disable_web_page_preview=True,
-                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text='🔍Sᴇᴀʀᴄʜ Hᴇʀᴇ🔍', url='https://t.me/+KI3avbmG5mZGMx')]])
+                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text='🔍Sᴇᴀʀᴄʜ Hᴇʀᴇ🔍', url=GRP_LNK)]])
             )
 @Client.on_message(filters.private & filters.text & filters.incoming)
 async def pm_text(bot, message):
