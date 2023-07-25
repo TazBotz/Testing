215977ea93690166abc14376bf0a32080925bdb0
RazAryanBot <136279522+RazAryanBot@users.noreply.github.com>
2023-07-25 09:17:30 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -45,7 +45,7 @@ async def save_group(bot, message):
                   ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
-            text=f"<b>Thankyou For Adding Me In {message.chat.title} ❣️\n\nIf you have any questions & doubts about using me contact support.</b>",
+            text=f"<b>Tʜᴀɴᴋʏᴏᴜ ꜰᴏʀ Aᴅᴅɪɴɢ ᴍᴇ\nɪɴ {message.chat.title} ❣️\n\nɪꜰ ʏᴏᴜ ʜᴀᴠᴇ ᴀɴʏ ǫᴜᴇꜱᴛɪᴏɴ\n&  ᴅᴏᴜʙᴛ ᴀʙᴏᴜᴛ ᴜꜱɪɴɢ ᴍᴇ\nᴄᴏɴᴛᴀᴄᴛ ꜱᴜᴘᴘᴏʀᴛ.</b>",
             reply_markup=reply_markup)
     else:
         settings = await get_settings(message.chat.id)
