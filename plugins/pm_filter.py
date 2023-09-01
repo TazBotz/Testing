52992a2f6f07f4aade93375117cd9acfce7e53fa
RazAryanBot <136279522+RazAryanBot@users.noreply.github.com>
2023-09-01 11:36:34 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1738,7 +1738,7 @@ async def auto_filter(client, msg, spoll=False):
             return
         if len(message.text) < 100:
             search = message.text
-            m=await message.reply_sticker("CAACAgUAAxkBAAEJzV5kvsGuaSJQWlV7wzWhvs07vKymGAACpRYAAjn5-FUV3KRIrRM2uy8E")
+            m=await message.reply_sticker("CAACAgUAAxkBAAEKLy1k8X4av59ToQawSHpcjVu2wyAcBwACtQkAAk9FgFdr5af3zLsITzAE")
             search = search.lower()
             find = search.split(" ")
             search = ""
@@ -1767,7 +1767,7 @@ async def auto_filter(client, msg, spoll=False):
     else:
         message = msg.message.reply_to_message  # msg will be callback query
         search, files, offset, total_results = spoll
-        m=await message.reply_sticker("CAACAgUAAxkBAAEJzV5kvsGuaSJQWlV7wzWhvs07vKymGAACpRYAAjn5-FUV3KRIrRM2uy8E")
+        m=await message.reply_sticker("CAACAgUAAxkBAAEKLy1k8X4av59ToQawSHpcjVu2wyAcBwACtQkAAk9FgFdr5af3zLsITzAE")
         settings = await get_settings(message.chat.id)
         await msg.message.delete()
     pre = 'filep' if settings['file_secure'] else 'file'
